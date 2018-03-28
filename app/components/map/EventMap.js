5bd0f5f75dfb776881b6b5b30ba9132bbf6ceee9
Pasi Lampinen <plam@fl027.local>
2018-03-28 17:17:31 +0300

    update event map callout styles

--- app/components/map/EventMap.js
+++ app/components/map/EventMap.js
@@ -8,7 +8,7 @@ import {
   Linking,
   Text,
   TouchableHighlight,
-  TouchableOpacity
+  TouchableOpacity,
 } from 'react-native';
 import MapView from 'react-native-maps';
 import { connect } from 'react-redux';
@@ -35,8 +35,8 @@ const CITY_COORDS = {
   },
   otaniemi: {
     latitude: 60.1841396,
-    longitude: 24.827895
-  }
+    longitude: 24.827895,
+  },
 };
 
 const MARKER_IMAGES = {
@@ -45,13 +45,12 @@ const MARKER_IMAGES = {
   TOILET: require('../../../assets/marker__toilet.png'),
   TAXI: require('../../../assets/marker__taxi.png'),
   STORE: require('../../../assets/marker__shop.png'),
-  ALKO: require('../../../assets/marker__wine.png')
+  ALKO: require('../../../assets/marker__wine.png'),
 };
 
 const VIEW_NAME = 'EventMap';
 
 class EventMap extends Component {
-
   componentDidMount() {
     this.props.dispatch(MarkerActions.fetchMarkers());
     analytics.viewOpened(VIEW_NAME);
@@ -71,31 +70,30 @@ class EventMap extends Component {
       component: EventDetail,
       name: event.name,
       model: event,
-      disableTopPadding: true
+      disableTopPadding: true,
     });
   }
 
   getCityRegion(city) {
     const deltaSettings = {
-     latitudeDelta: 0.2,
-     longitudeDelta: 0.2
-   };
-   const cityCoords = this.getCityCoords(city);
-   return Object.assign(deltaSettings, cityCoords);
+      latitudeDelta: 0.2,
+      longitudeDelta: 0.2,
+    };
+    const cityCoords = this.getCityCoords(city);
+    return Object.assign(deltaSettings, cityCoords);
   }
 
   @autobind
   getCityCoords(city) {
-   const cityName = city || this.props.currentCity;
-   const isTampere = (cityName || '').toLowerCase() === 'tampere';
-   return isTampere ? CITY_COORDS.tampere : CITY_COORDS.otaniemi;
+    const cityName = city || this.props.currentCity;
+    const isTampere = (cityName || '').toLowerCase() === 'tampere';
+    return isTampere ? CITY_COORDS.tampere : CITY_COORDS.otaniemi;
   }
 
   render() {
     const allEvents = [].concat(this.props.events.toJS());
 
-    const firstFutureEvent = _
-      .chain(allEvents)
+    const firstFutureEvent = _.chain(allEvents)
       .filter(item => time.isEventInFuture(item.endTime))
       .sortBy(item => time.getTimeStamp(item.endTime))
       .head()
@@ -107,7 +105,7 @@ class EventMap extends Component {
     });
 
     let locations = _.map(events, event => {
-      return _.merge({}, event, {type: 'EVENT'});
+      return _.merge({}, event, { type: 'EVENT' });
     });
     locations = locations.concat(this.props.markers.toJS());
 
@@ -116,36 +114,40 @@ class EventMap extends Component {
       return _.has(MARKER_IMAGES, location.type);
     });
     const markers = locations.map((location, i) => {
-      return <MapView.Marker
-        centerOffset={{x: 0, y: location.type === 'EVENT' ? 0 : 0}}
-        anchor={{x: 0.5, y: location.type === 'EVENT' ? 0.5 : 0.5}}
-        image={MARKER_IMAGES[location.type]}
-        key={i} coordinate={location.location}
-      >
-        {
-          location.type === 'EVENT'
+      return (
+        <MapView.Marker
+          centerOffset={{ x: 0, y: location.type === 'EVENT' ? 0 : 0 }}
+          anchor={{ x: 0.5, y: location.type === 'EVENT' ? 0.5 : 0.5 }}
+          image={MARKER_IMAGES[location.type]}
+          key={location.id}
+          coordinate={location.location}
+        >
+          {location.type === 'EVENT'
             ? this._renderEventMarker(location)
-            : this._renderStaticMarker(location)
-        }
-      </MapView.Marker>;
+            : this._renderStaticMarker(location)}
+        </MapView.Marker>
+      );
     });
 
-    if (disableMap){
-      return ( this._renderDisabledMapAnnouncement(firstFutureEvent) );
+    if (disableMap) {
+      return this._renderDisabledMapAnnouncement(firstFutureEvent);
     }
 
     const initialRegion = this.getCityRegion();
 
     return (
-      <View style={{flex:1}}>
-        <MapView style={styles.map}
+      <View style={{ flex: 1 }}>
+        <MapView
+          style={styles.map}
           initialRegion={initialRegion}
           showsUserLocation={Platform.OS === 'android' || this.props.locateMe}
           showsPointsOfInterest={false}
           showsBuildings={false}
           showsIndoors={false}
           rotateEnabled={false}
-          ref={(map) => { this.map = map; }}
+          ref={map => {
+            this.map = map;
+          }}
         >
           {markers}
         </MapView>
@@ -155,140 +157,150 @@ class EventMap extends Component {
 
         {this._renderFilterSelection()}
         {this._renderLocateMe()}
-
       </View>
     );
   }
 
-
   _renderDisabledMapAnnouncement(event) {
-    return (<View style={styles.emptyWrap}>
-      <View style={styles.emptyIconWrap}>
-        <MDIcon name="nature-people" style={styles.emptyIcon} />
+    return (
+      <View style={styles.emptyWrap}>
+        <View style={styles.emptyIconWrap}>
+          <MDIcon name="nature-people" style={styles.emptyIcon} />
+        </View>
+        <View style={styles.emptyContent}>
+          <Text style={styles.emptyTitle}>Oh noes!</Text>
+          <Text style={styles.emptyText}>
+            Event Map is not currently supported on your device. Be safe out there.
+          </Text>
+        </View>
       </View>
-      <View style={styles.emptyContent}>
-        <Text style={styles.emptyTitle}>Oh noes!</Text>
-        <Text style={styles.emptyText}>Event Map is not currently supported on your device. Be safe out there.</Text>
-      </View>
-    </View>);
+    );
   }
 
   _renderEventMarker(event) {
-    return <MapView.Callout onPress={() => this.onEventMarkerPress(event)} style={{ flex: 1, position: 'relative' }}>
-      <TouchableHighlight
-        underlayColor='transparent'
-        style={styles.calloutTouchable}
+    return (
+      <MapView.Callout
+        onPress={() => this.onEventMarkerPress(event)}
+        style={{ flex: 1, position: 'relative' }}
       >
-        <View style={styles.callout}>
-          <View>
-            <View style={styles.calloutTitleWrap}>
-              <Text style={styles.calloutTitle}>{event.name}</Text>
-              <Icon style={styles.calloutIcon} name='ios-arrow-forward' />
+        <TouchableHighlight underlayColor="transparent" style={styles.calloutTouchable}>
+          <View style={styles.callout}>
+            <View>
+              <View style={styles.calloutTitleWrap}>
+                <Text style={styles.calloutTitle}>{event.name}</Text>
+                <Icon style={styles.calloutIcon} name="ios-arrow-forward" />
+              </View>
+              <Text style={[styles.calloutInfo, { color: '#aaa', marginBottom: 10 }]}>
+                {time.getEventDay(event.startTime)}
+              </Text>
+              <Text style={styles.calloutInfo}>{event.locationName}</Text>
             </View>
-            <Text style={[styles.calloutInfo,{color:'#aaa', marginBottom:10}]}>
-              {time.getEventDay(event.startTime)}
-            </Text>
-            <Text style={styles.calloutInfo}>{event.locationName}</Text>
           </View>
-        </View>
-      </TouchableHighlight>
-    </MapView.Callout>;
+        </TouchableHighlight>
+      </MapView.Callout>
+    );
   }
 
   _renderStaticMarker(location) {
     let calloutProps = {};
     if (location.url) {
       calloutProps = {
-        onPress: () => Linking.openURL(location.url)
-      }
+        onPress: () => Linking.openURL(location.url),
+      };
     }
 
-    return <MapView.Callout {...calloutProps} style={{ flex: 1, position: 'relative' }}>
-      <TouchableHighlight
-        underlayColor='transparent'
-        style={styles.calloutTouchable}
-      >
-        <View style={styles.callout}>
-          {
-            location.url
+    return (
+      <MapView.Callout {...calloutProps} style={{ flex: 1, position: 'relative' }}>
+        <TouchableHighlight underlayColor="transparent" style={styles.calloutTouchable}>
+          <View style={styles.callout}>
+            {location.url
               ? this._renderStaticUrlMarkerView(location)
-              : this._renderStaticMarkerView(location)
-          }
-        </View>
-      </TouchableHighlight>
-    </MapView.Callout>;
+              : this._renderStaticMarkerView(location)}
+          </View>
+        </TouchableHighlight>
+      </MapView.Callout>
+    );
   }
 
   _renderStaticUrlMarkerView(location) {
-    return <View>
-      <View style={styles.calloutTitleWrap}>
-        <Text style={styles.calloutTitle}>{location.title}</Text>
-        <Icon style={styles.calloutIcon} name='ios-arrow-forward' />
-      </View>
+    return (
+      <View>
+        <View style={styles.calloutTitleWrap}>
+          <Text style={styles.calloutTitle}>{location.title}</Text>
+          <Icon style={styles.calloutIcon} name="ios-arrow-forward" />
+        </View>
 
-      <Text style={[styles.calloutInfo, {color:'#aaa'}]}>
-        {location.subtitle}
-      </Text>
-    </View>;
+        <Text style={[styles.calloutInfo, { color: '#aaa' }]}>{location.subtitle}</Text>
+      </View>
+    );
   }
 
   _renderStaticMarkerView(location) {
-    return <View>
-      <View style={styles.calloutTitleWrap}>
-        <Text style={[styles.calloutTitle, {color:'#333'}]}>
-          {location.title}
-        </Text>
-      </View>
+    return (
+      <View>
+        <View style={styles.calloutTitleWrap}>
+          <Text style={[styles.calloutTitle, { color: '#333' }]}>{location.title}</Text>
+        </View>
 
-      <Text style={[styles.calloutInfo, {color:'#aaa'}]}>
-        {location.subtitle}
-      </Text>
-    </View>;
+        <Text style={[styles.calloutInfo, { color: '#aaa' }]}>{location.subtitle}</Text>
+      </View>
+    );
   }
 
   // Enables swiping between tabs
   // 20px of right part of map can not be used to navigate map
   _renderSwipeHelperOverlay() {
-    return <View style={styles.swipeHelperArea}></View>;
+    return <View style={styles.swipeHelperArea} />;
   }
 
   _renderFilterSelection() {
-
-    const availableFilters = [
-     {id:'24H', title:'SOON'},
-     {id:'ALL', title:'ALL'}
-    ];
+    const availableFilters = [{ id: '24H', title: 'SOON' }, { id: 'ALL', title: 'ALL' }];
     return (
       <View style={styles.filterSelection}>
-      {
-        availableFilters.map((filter) => {
-          return this._renderFilterSelectionButton(filter)
-        })
-      }
+        {availableFilters.map(filter => {
+          return this._renderFilterSelectionButton(filter);
+        })}
       </View>
     );
   }
 
   _renderFilterSelectionButton(item) {
-    return <View key={item.id}>
-          <TouchableOpacity onPress={this._changeShowFilter.bind(this, item.id)}
-          style={styles.filterSelectionButton}>
-            <Text style={[styles.filterSelectionButtonText,
-              {color: this.props.showFilter === item.id ? theme.secondary : '#999' }]}>
-              {item.title}
-            </Text>
-          </TouchableOpacity>
-        </View>
+    return (
+      <View key={item.id}>
+        <TouchableOpacity
+          onPress={this._changeShowFilter.bind(this, item.id)}
+          style={styles.filterSelectionButton}
+        >
+          <Text
+            style={[
+              styles.filterSelectionButtonText,
+              { color: this.props.showFilter === item.id ? theme.secondary : '#999' },
+            ]}
+          >
+            {item.title}
+          </Text>
+        </TouchableOpacity>
+      </View>
+    );
   }
 
   _renderLocateMe() {
-    return Platform.OS === 'ios' ? <View style={styles.locateButton}>
-          <TouchableOpacity onPress={this._toggleLocateMe.bind(this,null)}
-            style={styles.locateButtonText} >
-            <MDIcon size={20} style={{ color:this.props.locateMe ? '#1D7BF7' : '#888' }} name='navigation' />
-          </TouchableOpacity>
-        </View> :
-        false;
+    return Platform.OS === 'ios' ? (
+      <View style={styles.locateButton}>
+        <TouchableOpacity
+          onPress={this._toggleLocateMe.bind(this, null)}
+          style={styles.locateButtonText}
+        >
+          <MDIcon
+            size={20}
+            style={{ color: this.props.locateMe ? '#1D7BF7' : '#888' }}
+            name="navigation"
+          />
+        </TouchableOpacity>
+      </View>
+    ) : (
+      false
+    );
   }
 
   _changeShowFilter(filterName) {
@@ -301,9 +313,11 @@ class EventMap extends Component {
 
   _maybeRenderLoading() {
     if (this.props.loading) {
-      return <View style={styles.loaderContainer}>
-        <Loader />
-      </View>;
+      return (
+        <View style={styles.loaderContainer}>
+          <Loader />
+        </View>
+      );
     }
 
     return false;
@@ -312,9 +326,11 @@ class EventMap extends Component {
   _isEventBetweenSelectedTime(event, firstFutureEvent) {
     switch (this.props.showFilter) {
       case '24H':
-        return firstFutureEvent &&
-               firstFutureEvent.endTime &&
-               time.eventsBetweenHours(event.endTime, firstFutureEvent.endTime, 24);
+        return (
+          firstFutureEvent &&
+          firstFutureEvent.endTime &&
+          time.eventsBetweenHours(event.endTime, firstFutureEvent.endTime, 24)
+        );
       default:
         return true;
     }
@@ -325,7 +341,7 @@ EventMap.propTypes = {
   navigator: PropTypes.object.isRequired,
   events: PropTypes.object.isRequired,
   markers: PropTypes.object.isRequired,
-}
+};
 
 const styles = StyleSheet.create({
   container: {
@@ -350,35 +366,36 @@ const styles = StyleSheet.create({
     right: 10,
   },
   callout: {
-    padding: 0,
+    paddingHorizontal: 0,
     flex: 1,
     flexGrow: 1,
-    justifyContent:'space-between',
-    alignItems:'flex-start',
-    flexDirection:'row'
+    justifyContent: 'space-between',
+    alignItems: 'flex-start',
+    flexDirection: 'row',
   },
   calloutTouchable: {
     padding: 6,
     flex: 1,
     flexGrow: 1,
   },
-  calloutTitleWrap:{
-    flex:1,
-    flexDirection:'row',
+  calloutTitleWrap: {
+    flex: 1,
+    flexDirection: 'row',
   },
-  calloutTitle:{
-    fontWeight:'bold',
-    color:theme.primary,
-    fontSize:12,
-    paddingRight:10,
+  calloutTitle: {
+    fontWeight: 'bold',
+    color: theme.secondary,
+    fontSize: 12,
+    paddingRight: 10,
   },
-  calloutInfo:{
-    fontSize:11,
+  calloutInfo: {
+    fontSize: 11,
+    color: theme.dark,
   },
-  calloutIcon:{
-    top:Platform.OS === 'ios' ? 0 : 2,
-    fontSize:14,
-    color:theme.primary
+  calloutIcon: {
+    top: Platform.OS === 'ios' ? 0 : 2,
+    fontSize: 14,
+    color: theme.secondary,
   },
   filterSelection: {
     position: 'absolute',
@@ -387,53 +404,53 @@ const styles = StyleSheet.create({
     flex: 1,
     flexDirection: 'row',
     backgroundColor: 'rgba(255,255,255,.8)',
-    elevation:2,
+    elevation: 2,
     shadowColor: '#000000',
     shadowOpacity: 0.25,
     shadowRadius: 1,
     shadowOffset: {
       height: 1,
-      width: 0
+      width: 0,
     },
 
     top: 12,
     left: 12,
     borderRadius: 3,
     height: 40,
-    width: 110
+    width: 110,
   },
   filterSelectionButton: {
     padding: 10,
     paddingLeft: 12,
-    paddingRight: 12
+    paddingRight: 12,
   },
   filterSelectionButtonText: {
     fontWeight: 'bold',
     fontSize: 11,
-    color:'#aaa'
+    color: '#aaa',
   },
-  locateButton:{
-    backgroundColor:'rgba(255,255,255,.8)',
+  locateButton: {
+    backgroundColor: 'rgba(255,255,255,.8)',
     shadowColor: '#000000',
     shadowOpacity: 0.25,
     shadowRadius: 1,
     shadowOffset: {
       height: 1,
-      width: 0
+      width: 0,
     },
-    borderRadius:2,
-    justifyContent:'center',
-    position:'absolute',
-    right:12,
-    top:12,
-    width:40,
-    height:40
+    borderRadius: 2,
+    justifyContent: 'center',
+    position: 'absolute',
+    right: 12,
+    top: 12,
+    width: 40,
+    height: 40,
   },
-  locateButtonText:{
-    flex:1,
-    alignItems:'center',
-    justifyContent:'center',
-    paddingTop:5
+  locateButtonText: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingTop: 5,
   },
   swipeHelperArea: {
     position: 'absolute',
@@ -443,21 +460,21 @@ const styles = StyleSheet.create({
     bottom: 0,
     elevation: 1,
     backgroundColor: 'transparent',
-    opacity: 0
+    opacity: 0,
   },
   emptyWrap: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#DDD'
+    backgroundColor: '#DDD',
   },
   emptyIconWrap: {
     alignItems: 'center',
-    justifyContent: 'center'
+    justifyContent: 'center',
   },
   emptyIcon: {
     color: '#bbb',
-    fontSize: 100
+    fontSize: 100,
   },
   emptyContent: {
     paddingTop: 10,
@@ -468,24 +485,24 @@ const styles = StyleSheet.create({
   emptyTitle: {
     fontSize: 30,
     textAlign: 'center',
-    color:'#666'
+    color: '#666',
   },
   emptyText: {
     textAlign: 'center',
-    color:'#999'
-  }
+    color: '#999',
+  },
 });
 
 const select = store => {
-
   return {
     currentCity: getCurrentCityName(store),
     locateMe: store.event.get('locateMe'),
     showFilter: store.event.get('showFilter'),
     events: store.event.get('list'),
     markers: store.marker.get('list'),
-    loading: store.event.get('listState') === LoadingStates.LOADING ||
-             store.marker.get('listState') === LoadingStates.LOADING
+    loading:
+      store.event.get('listState') === LoadingStates.LOADING ||
+      store.marker.get('listState') === LoadingStates.LOADING,
   };
 };
 
