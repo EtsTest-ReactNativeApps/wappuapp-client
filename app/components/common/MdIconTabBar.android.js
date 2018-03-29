84e0a5142e5b1a8432eaa2be3baa47fec9b21a4f
Pasi Lampinen <plam@fl027.local>
2018-03-29 15:02:02 +0300

    Android main navigation style changes

--- app/components/common/MdIconTabBar.android.js
+++ app/components/common/MdIconTabBar.android.js
@@ -3,14 +3,7 @@
 'use strict';
 
 import React from 'react';
-import {
-  StyleSheet,
-  Text,
-  TouchableNativeFeedback,
-  View,
-  Animated,
-  Easing
-} from 'react-native';
+import { StyleSheet, Text, TouchableNativeFeedback, View, Animated, Easing } from 'react-native';
 
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
@@ -19,49 +12,60 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
-    paddingBottom: 0
+    marginHorizontal: 0,
   },
   tabs: {
-    elevation: 4,
+    elevation: 5,
     height: 54,
     flexDirection: 'row',
     justifyContent: 'space-around',
     borderWidth: 0,
-    borderTopWidth: 1,
-    borderTopColor: '#f1f1f1'
+    borderTopWidth: 0,
+    borderTopColor: '#f1f1f1',
   },
   textLabel: {
-    fontSize: 10,
-    textAlign:'center',
-    // position:'absolute',
-    left:0,
-    right:0,
-    bottom: 0
-  }
+    fontSize: 11,
+    textAlign: 'center',
+    position: 'absolute',
+    marginTop: 2,
+    left: 0,
+    right: 0,
+    bottom: 7,
+  },
 });
 
-
+const AnimatedIcon = Animated.createAnimatedComponent(Icon);
 var AndroidTabBar = React.createClass({
   propTypes: {
     goToPage: React.PropTypes.func,
     activeTab: React.PropTypes.number,
     tabs: React.PropTypes.array,
-    backgroundColor : React.PropTypes.string,
-    activeTextColor : React.PropTypes.string,
-    inactiveTextColor : React.PropTypes.string,
+    backgroundColor: React.PropTypes.string,
+    activeTextColor: React.PropTypes.string,
+    inactiveTextColor: React.PropTypes.string,
   },
 
   getInitialState() {
+    const { activeTab } = this.props;
     return {
-      buttonAnimations: this.props.tabs.map(() => new Animated.Value(1))
+      buttonAnimations: this.props.tabs.map(
+        (t, index) => new Animated.Value(index === activeTab ? 1 : 0)
+      ),
     };
   },
 
   componentWillReceiveProps(nextProps) {
     const { buttonAnimations } = this.state;
-    if (nextProps.activeTab !== this.props.activeTab) {
-      buttonAnimations.map(b => b.setValue(0));
-      Animated.timing(buttonAnimations[nextProps.activeTab], { duration: 330, easing: Easing.ease, toValue: 1}).start();
+    const { activeTab } = this.props;
+    if (nextProps.activeTab !== activeTab) {
+      buttonAnimations.map(b => {
+        b.setValue(0);
+      });
+      Animated.timing(buttonAnimations[nextProps.activeTab], {
+        duration: 250,
+        easing: Easing.ease,
+        toValue: 1,
+      }).start();
     }
   },
 
@@ -82,39 +86,43 @@ var AndroidTabBar = React.createClass({
     const buttonAnimation = this.state.buttonAnimations[page];
 
     return (
-    <TouchableNativeFeedback
-      key={item.title}
-      onPress={() => this.props.goToPage(page)}
-      background={TouchableNativeFeedback.SelectableBackground()}
-      delayPressIn={0}
-    >
-      <View style={[styles.tab, { paddingLeft: isTabActive ? 0 : 0, paddingRight: isTabActive ? 0 : 0 }]}>
-        <Icon
-          name={item.icon}
-          size={item.iconSize || 22}
-          style={{
-            top: 0, //isTabActive ? -7 : 0,
-            color: isTabActive ? activeTextColor : inactiveTextColor,
-          }} />
+      <TouchableNativeFeedback
+        key={item.title}
+        onPress={() => this.props.goToPage(page)}
+        background={TouchableNativeFeedback.SelectableBackgroundBorderless()}
+        delayPressIn={0}
+      >
+        <View style={styles.tab}>
+          <AnimatedIcon
+            name={item.icon}
+            size={item.iconSize || 22}
+            style={{
+              bottom: buttonAnimation.interpolate({ inputRange: [0, 1], outputRange: [0, 8] }),
+              color: isTabActive ? activeTextColor : inactiveTextColor,
+            }}
+          />
 
-        {item.title && isTabActive &&
-          <Animated.Text style={[
-            styles.textLabel,
-            {
-              color: activeTextColor,
-              opacity: isTabActive ? buttonAnimation : 1,
-              transform: [{
-                scale: isTabActive ? buttonAnimation : 0
-                // scale: isTabActive ? 1.1 : 0
-              }]
-            }
-          ]}>
-          {item.title}
-          </Animated.Text>
-        }
-
-      </View>
-    </TouchableNativeFeedback>
+          {item.title &&
+            isTabActive && (
+              <Animated.Text
+                style={[
+                  styles.textLabel,
+                  {
+                    color: activeTextColor,
+                    opacity: buttonAnimation,
+                    transform: [
+                      {
+                        scale: buttonAnimation,
+                      },
+                    ],
+                  },
+                ]}
+              >
+                {item.title}
+              </Animated.Text>
+            )}
+        </View>
+      </TouchableNativeFeedback>
     );
   },
 
@@ -127,7 +135,7 @@ var AndroidTabBar = React.createClass({
           {tabs.map((tab, i) => this.renderTabOption(tab, i))}
         </View>
       </View>
-      );
+    );
   },
 });
 
