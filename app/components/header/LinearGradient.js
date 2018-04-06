fd0b3e741d2488a8de5516518c83ff0a09b4a26a
Pasi Lampinen <plam@fl027.local>
2018-04-06 13:48:46 +0300

    Different Linear Gradient props for Android
    Behaves a bit differently with Android

--- app/components/header/LinearGradient.js
+++ app/components/header/LinearGradient.js
@@ -5,15 +5,16 @@ import theme from '../../style/theme';
 import { IOS } from '../../services/device-info';
 
 const colorfulSet = [theme.secondary, theme.secondaryLight, theme.secondaryTint];
-// const colorfulSet = [theme.white, theme.white, theme.white];
 const singleColorSet = [theme.secondary, theme.secondary, theme.secondary];
 
-const defaultLocations = IOS ? [0, 0.4, 1] : [0, 0.6, 1];
+const defaultLocations = IOS ? [0, 0.4, 1] : [0, 0.5, 1];
+const start = IOS ? { x: 0, y: 0.25 } : { x: 0.3, y: 0.25 };
+const end = IOS ? { x: 1, y: 1 } : { x: 0.7, y: 1 };
 
 const LG = ({ singleColor, ...props }) => (
   <LinearGradient
-    start={{ x: 0.0, y: 0.5 }}
-    end={{ x: 1, y: 1 }}
+    start={start}
+    end={end}
     locations={defaultLocations}
     colors={singleColor ? singleColorSet : colorfulSet}
     {...props}
