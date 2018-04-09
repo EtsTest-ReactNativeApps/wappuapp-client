ba92d9cd931326c929e80f7c16c4b1ffb9f2f073
Pasi Lampinen <plam@fl027.local>
2018-04-09 17:23:05 +0300

    hide comment action

--- app/reducers/competition.js
+++ app/reducers/competition.js
@@ -1,5 +1,5 @@
 'use strict';
-
+import { createSelector } from 'reselect';
 import Immutable from 'immutable';
 import {
   POST_ACTION_REQUEST,
@@ -16,8 +16,9 @@ import {
   HIDE_NOTIFICATION,
   UPDATE_COOLDOWNS,
   SET_EDITABLE_IMAGE,
-  CLEAR_EDITABLE_IMAGE
+  CLEAR_EDITABLE_IMAGE,
 } from '../actions/competition';
+import ActionTypes from '../constants/ActionTypes';
 
 const initialState = Immutable.fromJS({
   isSending: false,
@@ -34,7 +35,7 @@ const initialState = Immutable.fromJS({
   editableImage: null,
 });
 
-const getDisabledActions = (state) => {
+const getDisabledActions = state => {
   // Called once a second from FeedList (UPDATE_COOLDOWNS action)
   // - go through all cooldownTimes
   // - compare them to current time
@@ -55,6 +56,15 @@ const getDisabledActions = (state) => {
     }, Immutable.List());
 };
 
+export const getActionTypes = store => store.competition.get('actionTypes');
+
+const hiddenActionTypes = [ActionTypes.COMMENT];
+export const getActionTypesForFeed = createSelector(getActionTypes, actionTypes => {
+  let feedActionTypes = actionTypes.filter(type => hiddenActionTypes.indexOf(type.get('code')) < 0);
+
+  return feedActionTypes;
+});
+
 export default function competition(state = initialState, action) {
   switch (action.type) {
     case OPEN_TEXTACTION_VIEW:
@@ -68,7 +78,7 @@ export default function competition(state = initialState, action) {
     case POST_ACTION_REQUEST:
       return state.merge({
         isSending: true,
-        isError: false
+        isError: false,
       });
     case POST_ACTION_SUCCESS:
       const actionType = state
@@ -82,33 +92,33 @@ export default function competition(state = initialState, action) {
     case POST_ACTION_FAILURE:
       return state.merge({
         isSending: false,
-        isError: true
+        isError: true,
       });
     case GET_ACTION_TYPES_REQUEST:
       return state.merge({
         isLoadingActionTypes: true,
-        isErrorLoadingActionTypes: false
+        isErrorLoadingActionTypes: false,
       });
     case GET_ACTION_TYPES_SUCCESS:
       return state.merge({
         isLoadingActionTypes: false,
         isErrorLoadingActionTypes: false,
-        actionTypes: action.payload
+        actionTypes: action.payload,
       });
     case GET_ACTION_TYPES_FAILURE:
       return state.merge({
         isLoadingActionTypes: false,
-        isErrorLoadingActionTypes: true
+        isErrorLoadingActionTypes: true,
       });
     case SHOW_NOTIFICATION:
       return state.merge({
         isNotificationVisible: true,
-        notificationText: action.payload
+        notificationText: action.payload,
       });
     case HIDE_NOTIFICATION:
       return state.merge({
         isNotificationVisible: false,
-        notificationText: ''
+        notificationText: '',
       });
     case UPDATE_COOLDOWNS:
       return state.set('disabledActionTypes', getDisabledActions(state));
