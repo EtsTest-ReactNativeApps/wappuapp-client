5b149ad76b3b83b1e95df3a2490105b0c6d48456
Pasi Lampinen <plam@dyn-100-68.wlan.futurice.org>
2018-04-11 11:20:46 +0300

    Hotfix for word cutting bug
    Oneplus Slate font is buggy with react-native. Some words are being cut.
    
    This fixes the bug for now.
    More info https://github.com/facebook/react-native/issues/15114

--- index.android.js
+++ index.android.js
@@ -1,5 +1,24 @@
 'use strict';
-import React, { AppRegistry, UIManager } from 'react-native';
+import React from 'react';
+import { AppRegistry, UIManager, StyleSheet, Text } from 'react-native';
 import RootView from './app/containers/RootView';
-UIManager.setLayoutAnimationEnabledExperimental && UIManager.setLayoutAnimationEnabledExperimental(true);
+UIManager.setLayoutAnimationEnabledExperimental &&
+  UIManager.setLayoutAnimationEnabledExperimental(true);
 AppRegistry.registerComponent('wappuapp', () => RootView);
+
+// HOTFIX
+// Text will be cut on some devices
+// https://github.com/facebook/react-native/issues/15114
+const styles = StyleSheet.create({
+  defaultFontFamily: {
+    fontFamily: 'Roboto',
+  },
+});
+
+const oldRender = Text.prototype.render;
+Text.prototype.render = function(...args) {
+  const origin = oldRender.call(this, ...args);
+  return React.cloneElement(origin, {
+    style: [styles.defaultFontFamily, origin.props.style],
+  });
+};
